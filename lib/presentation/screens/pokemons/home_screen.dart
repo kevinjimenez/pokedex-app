d9c7b4664c4221beca23c26ab2dd742d7b435b2b
Kevin Jimenez <kjimenez@creditu.com>
2023-07-23 19:15:45 -0500

    feat: listview falta para consumir api

--- lib/presentation/screens/pokemons/home_screen.dart
+++ lib/presentation/screens/pokemons/home_screen.dart
@@ -6,6 +6,28 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Pokedex'),
+      ),
+      body: ListView.builder(
+        // itemCount: ,
+        itemBuilder: (context, index) {
+          // return Row(
+          //   children: [
+          //     Container(
+          //       color: Colors.red,
+          //       child: Text('data'),
+          //     ),
+          //     Container(
+          //       color: Colors.red,
+          //       child: Text('data'),
+          //     )
+          //   ],
+          // );
+          return Text('data');
+        },
+      ),
+    );
   }
 }
