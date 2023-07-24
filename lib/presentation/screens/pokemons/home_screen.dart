a5f68bf39a4bcf93230b27f5884bb4eeb7ba3d64
Kevin Jimenez <kjimenez@creditu.com>
2023-07-23 21:52:24 -0500

    feat: uso de grip view

--- lib/presentation/screens/pokemons/home_screen.dart
+++ lib/presentation/screens/pokemons/home_screen.dart
@@ -23,37 +23,78 @@ class _HomeView extends ConsumerStatefulWidget {
 }
 
 class HomeViewState extends ConsumerState<_HomeView> {
-
-
   @override
   void initState() {
     super.initState();
     ref.read(getPokemonsProvider.notifier).loadPokemos();
   }
 
-
   @override
   Widget build(BuildContext context) {
     final pokemons = ref.watch(getPokemonsProvider);
-    return ListView.builder(
-      itemCount: pokemons.length,
-      physics: const BouncingScrollPhysics(),
-      itemBuilder: (context, index) {
-        final pokemon = pokemons[index];
-        // return Row(
-        //   children: [
-        //     Container(
-        //       color: Colors.red,
-        //       child: Text('data'),
-        //     ),
-        //     Container(
-        //       color: Colors.red,
-        //       child: Text('data'),
-        //     )
-        //   ],
-        // );
-        return Text(pokemon.name);
-      },
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 15),
+      child: GridView.builder(
+        itemCount: pokemons.length,
+        gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
+            maxCrossAxisExtent: 200,
+            childAspectRatio: 3 / 2,
+            crossAxisSpacing: 10,
+            mainAxisSpacing: 10),
+        itemBuilder: (BuildContext context, int index) {
+          final pokemon = pokemons[index];
+          final no = index + 1;
+          return GestureDetector(
+            onTap: () {
+              print(no);
+            },
+            child: ClipRRect(
+                borderRadius: BorderRadius.circular(10),
+                child: Container(
+                    padding:
+                        const EdgeInsets.symmetric(horizontal: 5, vertical: 5),
+                    color: Colors.orange,
+                    child: Stack(
+                      children: [
+                        //* foto pokemon
+                        Positioned(
+                          right: 0,
+                          top: 25,
+                          child: Image.network(
+                            'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/$no.png',
+                            fit: BoxFit.cover,
+                            loadingBuilder: (context, child, loadingProgress) {
+                              if (loadingProgress != null) {
+                                return const Center(
+                                  child: CircularProgressIndicator(
+                                    strokeWidth: 2,
+                                  ),
+                                );
+                              }
+                              return child;
+                            },
+                          ),
+                        ),
+                        //* no pokemon
+                        Positioned(
+                          bottom: 0,
+                          right: 0,
+                          child: Text(
+                            '#$no',
+                            style: const TextStyle(color: Colors.white),
+                          ),
+                        ),
+                        //* nombre pokemon
+                        Text(
+                          pokemon.name,
+                          style: const TextStyle(
+                              fontSize: 18, color: Colors.white),
+                        ),
+                      ],
+                    ))),
+          );
+        },
+      ),
     );
   }
 }
