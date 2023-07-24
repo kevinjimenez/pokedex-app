730cf797aed95977c923ead13b5b96ac4dd45555
Kevin Jimenez <kjimenez@creditu.com>
2023-07-23 19:06:31 -0500

    feat: tema material use

---
+++ lib/config/theme/app_theme.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+
+class AppTheme {
+  ThemeData getTheme() => ThemeData(useMaterial3: true);
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:pokedex/config/theme/app_theme.dart';
 
 void main() => runApp(const MyApp());
 
@@ -8,6 +9,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: AppTheme().getTheme(),
       title: 'Material App',
       home: Scaffold(
         appBar: AppBar(
